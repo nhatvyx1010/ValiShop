253172f1acda807e964872019156cf522ef8d0fb
Nguyễn Phạm Nhật Vỹ <115072818+nhatvyx1010@users.noreply.github.com>
2023-10-19 22:05:13 +0700

    Add files via upload

---
+++ MyShop/MyShop/Controllers/AccessController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyShop.Models;
+namespace MyShop.Controllers
+{
+    public class AccessController : Controller
+    {
+        MyShopContext db = new MyShopContext();
+        [HttpGet]
+        public IActionResult Login()
+        {
+            if (HttpContext.Session.GetString("UserName") == null)
+            {
+                return View();
+            }
+            return RedirectToAction("Index", "Home");
+        }
+        [HttpPost]
+        public IActionResult Login(TUser user)
+        {
+            if (HttpContext.Session.GetString("UserName")==null) 
+            { 
+                var u=db.TUsers.Where(x=>x.Username.Equals(user.Username) && x.Password.Equals(user.Password)).FirstOrDefault();
+                if (u!=null)
+                {
+                    HttpContext.Session.SetString("UserName", u.Username.ToString());
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View();
+        }
+    }
+}
